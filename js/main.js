a9325d3a46f4b16404e51a43498bad5e12bbe016
johanuribe03 <johan.uribe@gmail.com>
2022-11-11 03:23:18 +0000

    AddProductToCart Function and Buy Button Function

--- js/main.js
+++ js/main.js
@@ -40,6 +40,17 @@ function ready() {
         var button = addCart[i];
         button.addEventListener("click", addCartClicked);
     }
+    // Buy Button Work
+    document.getElementsByClassName("btn-buy")[0].addEventListener("click", buyButtonClicked);
+
+}
+// Buy Button
+function buyButtonClicked() {
+    alert('Your Order is placed');
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    while (cartContent.hasChildNodes()) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
 }
 
 //Remove Items From Cart
@@ -69,19 +80,21 @@ function addCartClicked(e){
 }
 function addProductToCart (title, price, productImg) {
     var cartShopBox = document.createElement("div");
-    cartShopBox.classList.add('cart-box');
+    cartShopBox.classList.add("cart-box");
     var cartItems = document.getElementsByClassName("cart-content")[0];
     var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
     for (var i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title) {
         alert ("You have already add this item to cart");
         return;
+        }
     }
-}
+
     var cartBoxContent = `
-        <img src="img/product2.jpg" alt="" class="cart-img">
+        <img src="${productImg}" alt="" class="cart-img">
         <div class="detail-box">
-            <div class="cart-product-title">Earbuds</div>
-            <div class="cart-price">$25.05</div>
+            <div class="cart-product-title">${title}</div>
+            <div class="cart-price">${price}</div>
             <input type="number" value="1" class="cart-quantity">
         </div>
         <!-- Remove cart -->
@@ -93,7 +106,7 @@ function addProductToCart (title, price, productImg) {
     cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
     cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
     
-    
+}
 // Update Total
 function updatetotal() {
     var cartContent = document.getElementsByClassName("cart-content")[0];
